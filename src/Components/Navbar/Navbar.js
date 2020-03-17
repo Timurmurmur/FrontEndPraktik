53bf5d86927a6c871e3704bd2e7216605e4314a3
tw9ntyseven <vladakmilicin@mail.ru>
2020-03-17 16:58:00 +0300

    add icons

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import './Navbar.css';
+import { HomeFilled, UserOutlined, ImportOutlined } from '@ant-design/icons';
 
 const img = require('../../assets/Frame.png');
 
 export const Navbar = () => {
     return (
         <div className="navBarWrapper">
+        <span className="logo">
             <img src={img} />
-            
+        </span>
+        <div className="navBarItems">
+            <HomeFilled />
+            <UserOutlined />
+            <ImportOutlined />
+        </div>
         </div>
     );
 }
